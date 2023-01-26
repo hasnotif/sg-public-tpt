99ebe9dc94f1e8ed9edd7aacef7ec91a783406db
hasnotif <hasifirsyaad@gmail.com>
2023-01-26 23:22:05 +0800

    simplified data folder

--- public_tpt_dataminer.py
+++ public_tpt_dataminer.py
@@ -1,1 +1,32 @@
-print("Hello World")
+import json
+import urllib
+from urllib.parse import urlparse
+
+import httplib2 as http
+
+def main():
+    #Authentication parameters
+    headers = {'AccountKey': 'T/uOGZ60RV+ySexzg5g1Tg==', 'accept': 'application/json'}
+
+    #API parameters
+    url = 'http://datamall2.mytransport.sg/'
+    path = 'ltaodataservice/PV/Bus'
+
+    #Build query string & specify type of API call
+    target = urlparse(url + path)
+    print(target.geturl())
+    method = 'GET'
+    body = ''
+
+    #Get handle to http
+    h = http.Http()
+ 
+    #Obtain results
+    response, content = h.request(target.geturl(), method, body, headers)
+
+    #Parse JSON to print
+    jsonObj = json.loads(content)
+    print(json.dumps(jsonObj, sort_keys=True, indent=4))
+
+if __name__ == "__main__":
+    main()
